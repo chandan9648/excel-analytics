21bfa11b3e58dc859bae6292ad4540a016d55701
chandan9648 <chandankkumar156@gmail.com>
2025-07-11 22:38:30 +0530

    adjusting home layout

--- frontend/src/pages/auth/Home.jsx
+++ frontend/src/pages/auth/Home.jsx
@@ -6,7 +6,7 @@ const Home = () => {
   const { user, logout } = useContext(AuthContext);
 
   return (
-    <div className="min-h-screen bg-blue-100 flex items-center justify-center">
+    <div className="min-h-screen bg-green-100 flex items-center justify-center">
       <div className="max-w-6xl w-full grid grid-cols-1 md:grid-cols-2 gap-8 items-center p-8">
         
         {/* Left Side: Image */}
@@ -14,13 +14,13 @@ const Home = () => {
           <img
             src="https://plus.unsplash.com/premium_photo-1720091339077-d0f56397a0c9?q=80&w=1332&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
             alt="Excel Analytics"
-            className="w-full max-w-md md:max-w-150 h-100 rounded shadow-lg"
+            className="w-full max-w-md md:max-w-170 h-100 rounded ml-20 shadow-lg"
           />
         </div>
 
         {/* Right Side: Welcome content */}
-        <div className="bg-white p-18 w-100 ml-20 rounded shadow text-center">
-          <h1 className="text-4xl font-bold mb-5 text-green-700">Excel Analytics</h1>
+        <div className="bg-white p-18 w-100 ml-20 rounded shadow-2xl text-center">
+          <h1 className="text-4xl font-bold mb-5 text-green-800">Excel Analytics</h1>
           
           {user ? (
             <>
