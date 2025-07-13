d19ed7e2b718ea336f5a836445bd84a954012848
chandan9648 <chandankkumar156@gmail.com>
2025-07-13 12:21:25 +0530

    lgin and logout authentication completed

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -3,8 +3,7 @@ import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 
 
-
-// Signup controller
+// signup controller
 export const signup = async (req, res) => {
   try {
     const { name, email, password, role = 'user' } = req.body;
@@ -13,59 +12,79 @@ export const signup = async (req, res) => {
       return res.status(400).json({ msg: 'All fields are required' });
     }
 
-    const existing = await User.findOne({ email });
-    if (existing) return res.status(400).json({ msg: 'User already exists' });
+    const trimmedEmail = email.toLowerCase().trim();
+    const trimmedPassword = password.trim();
+    const existing = await User.findOne({ email: trimmedEmail });
 
-    const hashed = await bcrypt.hash(password, 10);
+    if (existing) {
+      return res.status(400).json({ msg: 'User already exists' });
+    }
 
+    
     const user = await User.create({
-      name, 
-      email,
-      password: hashed,
+      name,
+      email: trimmedEmail,
+      password: trimmedPassword,
       role,
     });
 
-    res.status(201).json({
-      msg: 'User signuped!',
-      data: { role: user.role }
-    });
-  } catch (err) {
-    res.status(500).json({ msg: err.message });
-  }
-};
-
-// LOGIN controller
-export const login = async (req, res) => {
-  try {
-    const { email, password } = req.body;
-
-    const user = await User.findOne({ email: email.toLowerCase() });
-    if (!user) return res.status(400).json({ msg: 'Invalid credential' });
-
-    const isMatch = await bcrypt.compare(password, user.password);
-    if (!isMatch) return res.status(400).json({ msg: 'Invalid email / password' });
-
-    const token = jwt.sign(
-      {
-        id: user._id,
-        name: user.name,
-        email: user.email,
-        role: user.role
-      },
-      process.env.JWT_SECRET,
-      { expiresIn: '1h' }
-    );
-
-    res.json({
-      token,
-      user: {
-        id: user._id,
-        name: user.name,
-        email: user.email,
-        role: user.role
+    return res.status(201).json({
+      msg: 'User registered successfully',
+      data: {
+       role: user.role
       }
     });
   } catch (err) {
     res.status(500).json({ msg: err.message });
   }
 };
+
+
+// login controlller
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const trimmedEmail = email.toLowerCase().trim();
+    const trimmedPassword = password.trim();
+
+    const user = await User.findOne({ email: trimmedEmail });
+      //  console.log("User found:", user);
+
+    if (!user) {
+      return res.status(400).json({ msg: 'User not found' });
+    }
+
+
+    const isMatch = await bcrypt.compare(trimmedPassword, user.password);
+      //  console.log("Password Match:", isMatch);
+
+    if (!isMatch) {
+      return res.status(400).json({ msg: 'Invalid email/password' });
+    }
+
+    const token = jwt.sign(
+      {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+      },
+      process.env.JWT_SECRET,
+      { expiresIn: '1h' }
+    );
+
+    return res.json({
+      msg: 'Login successful',
+      token,
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+      }
+    });
+  } catch (err) {
+    res.status(500).json({ msg: err.message });
+  }
+};
+
+
+
